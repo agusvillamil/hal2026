f37bde755cd14bfa6a96c80605bde34ec0efee1c
Agustin Villamil <villa.09.agus@gmail.com>
2026-03-26 16:05:34 -0300

    Configuracion de workflow para github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === "true"
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: isGithubPages ? "/hal2026" : "",
+  assetPrefix: isGithubPages ? "/hal2026/" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
